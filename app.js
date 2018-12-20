22771f6185263c5187967d4f856ebf65b3548dc4
mk <mk@mks-MacBook-Pro.local>
2018-12-20 14:17:25 -0500

    Updates: codelab updates

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var people = require('./routes/people');
+var codelab = require('./routes/codelab');
 
 var app = express();
+require('dotenv').config();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/people', people);
+app.use('/codelab', codelab);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
